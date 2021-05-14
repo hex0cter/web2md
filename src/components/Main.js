5bc46cce6a4a4cc6a7f2324c3a7162a57bc40451
Daniel Han <daniel.han@scrive.com>
2021-05-14 19:58:26 +0200

    Restructure files

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="Main">
+      <header className="Main-header">
+        <p>
+          Edit <code>src/Main.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
