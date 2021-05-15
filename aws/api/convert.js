46fe1428b4c4ae671e24901e1b1258d8778c487f
Daniel Han <daniel.han@scrive.com>
2021-05-15 12:50:42 +0200

    First stage works

--- aws/api/convert.js
+++ aws/api/convert.js
@@ -1,7 +1,12 @@
+var Mercury = require('@postlight/mercury-parser');
 
 exports.handler = async (event) => {
   console.log('event', event);
   const { url } = JSON.parse(event.body)
+  const result = await Mercury.parse(url);
+  console.log(`result: ${JSON.stringify(result)}`);
+  const payload = { url, result };
+  console.log(`payload: ${JSON.stringify(payload)}`);
 
   const response = {
     statusCode: 200,
@@ -9,7 +14,7 @@ exports.handler = async (event) => {
       'Access-Control-Allow-Origin' : '*',
       'Access-Control-Allow-Credentials' : true
     },
-    body: JSON.stringify({ url, result: 'ok' })
+    body: JSON.stringify(payload)
   };
 
   return response;
