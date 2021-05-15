48daa8c3b482cd4b6c8a34a15a3823b66d9f5295
Daniel Han <daniel.han@scrive.com>
2021-05-15 22:54:59 +0200

    Better layout

--- src/components/ResultPanel/index.js
+++ src/components/ResultPanel/index.js
@@ -1,17 +1,13 @@
 import * as React from 'react';
-import ReactMarkdown from 'react-markdown';
-import { useStateContext } from '../../stateContext';
+import Preview from '../Preview';
+import Editable from '../Editable';
 import './index.css';
 
 function ResultPanel() {
-  const {
-    state: { markdown },
-  } = useStateContext();
-
   return (
     <div className="ResultPanel">
-      <ReactMarkdown>{ markdown }</ReactMarkdown>
-      {/* Here comes the result: { JSON.stringify(markdown) } */}
+      <Editable />
+      <Preview />
     </div>
   );
 }
